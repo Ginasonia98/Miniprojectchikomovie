8cd9b31b4e71f79e5ff886300cadc6e11ded8d70
Ginachiko1998 <soniagina669@gmail.com>
2022-11-28 17:56:56 +0700

    milestone1

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiKey = process.env.REACT_APP_APIKEY;
+const baseURL = process.env.REACT_APP_BASEURL;
+
+export const getMovieList = async () => {
+  console.log("apikey", process.env.REACT_APP_APIKEY);
+  const movie = await axios.get(`${baseURL}/movie/popular?page=1&api_key=${apiKey}`);
+  return movie.data.results;
+};
+
+export const searchMovie = async (q) => {
+  const search = await axios.get(`${baseURL}/search/movie?query=${q}&page=1api_key=${apiKey}`);
+  return search.data
+}
